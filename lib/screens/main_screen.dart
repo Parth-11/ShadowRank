0dca7400a601802503b4e3f430f8ecd8ddb26491
Parth-11 <parth12.mgg@gmail.com>
2024-07-01 23:31:21 +0530

    Completed bottom navigation bar in main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,32 +1,48 @@
-import 'package:architect_system_app/components/background.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   static const String id = 'Main Screen';
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  void _selectionBottomNavigation(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _pages = [Column(), Column(), Column()];
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: background,
-      child: Scaffold(
+    return Scaffold(
+      body: _pages.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.transparent,
-        body: const Column(),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.transparent,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.track_changes_outlined),
-              label: 'Quest',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.line_style_outlined), label: 'Attributes'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-        ),
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.cyan.shade700,
+        onTap: _selectionBottomNavigation,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.track_changes_outlined),
+            label: 'Quest',
+            backgroundColor: Colors.lightBlueAccent.shade200,
+          ),
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.line_style_outlined),
+              label: 'Attributes',
+              backgroundColor: Colors.lightBlueAccent.shade200),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.person),
+            label: 'Profile',
+            backgroundColor: Colors.lightBlueAccent.shade200,
+          ),
+        ],
       ),
     );
   }
