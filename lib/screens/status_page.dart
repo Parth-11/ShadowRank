b4a65537854f76e740c2289625ec7f692532eb40
bhumika2856 <bhumikakundra28@gmail.com>
2024-07-12 05:28:25 +0530

    status page created

---
+++ lib/screens/status_page.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+
+class StatusPage extends StatelessWidget {
+  const StatusPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+      
+      ),
+      backgroundColor: Color.fromARGB(255, 88, 105, 112),
+      appBar: AppBar(
+        title:Text("status"),
+      ),
+    );
+  }
+}
