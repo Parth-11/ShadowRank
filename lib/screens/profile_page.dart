58af3e3fd4f576ccd4f4c2918871cfa778ad9e3c
Parth-11 <parth12.mgg@gmail.com>
2024-07-11 23:42:28 +0530

    Added avatar and name and level details to profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,37 +1,95 @@
-import 'package:architect_system_app/components/button.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:stroke_text/stroke_text.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        const CircleAvatar(),
-        const Text('Name'),
-        const Text('Email'),
-        const Text('Age'),
-        const Text('Highest attribute'),
-        const Text('Friends'),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Button(
-                buttonText: 'Add Friends',
-                onPress: () {},
+    return SafeArea(
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/profilePage_bg.png')),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: Material(
+                  borderRadius: BorderRadius.circular(16.0),
+                  elevation: 10,
+                  color: HexColor('4355F5').withOpacity(0.5),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    height: 40,
+                    minWidth: 20,
+                    child: const Icon(Icons.edit),
+                  ),
+                )),
+            const CircleAvatar(
+              radius: 120,
+              child: Image(image: AssetImage('assets/images/Avatar.png')),
+            ),
+            Container(
+              height: 90,
+              width: 269,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadiusDirectional.all(
+                  Radius.circular(20),
+                ),
+                color: HexColor('4355F5').withOpacity(0.28),
               ),
-              Button(
-                buttonText: 'Edit Profile',
-                onPress: () {},
-              )
-            ],
-          ),
-        )
-      ],
+              child: const Column(
+                children: [
+                  StrokeText(
+                    text: 'Sun Jin Woo',
+                    textStyle: TextStyle(
+                      fontFamily: 'Solo Level',
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                    strokeColor: Colors.black,
+                    strokeWidth: 3,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      StrokeText(
+                        text: 'Level',
+                        textStyle: TextStyle(
+                          fontFamily: 'Solo Level',
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                        strokeColor: Colors.black,
+                        strokeWidth: 3,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      StrokeText(
+                        text: '20',
+                        textStyle: TextStyle(
+                          fontFamily: 'Solo Level',
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                        strokeColor: Colors.black,
+                        strokeWidth: 3,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
