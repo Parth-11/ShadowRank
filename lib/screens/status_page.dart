9377bd4e2265b98eee92c9ef4e7ab2801a8cfcae
bhumika2856 <bhumikakundra28@gmail.com>
2024-07-12 05:40:09 +0530

    .

--- lib/screens/status_page.dart
+++ lib/screens/status_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import "package:flutter/material.dart";
 
 class StatusPage extends StatelessWidget {
@@ -8,12 +10,24 @@ class StatusPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:Container(
+        width:double.infinity,
+        height: 100,
+        child: Center(
+          child: Text(
+            'STATUS',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+        ),
       
       ),
       backgroundColor: Color.fromARGB(255, 88, 105, 112),
       appBar: AppBar(
         title:Text("status"),
       ),
+      
     );
   }
 }
