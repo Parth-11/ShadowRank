d0d5b610b48ef84bf20c5789dcd55f1fda0511e0
bhumika2856 <bhumikakundra28@gmail.com>
2024-07-16 23:29:54 +0530

    Your commit message

--- lib/screens/status_page.dart
+++ lib/screens/status_page.dart
@@ -11,7 +11,6 @@ class StatusPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: const Text("Status"),
