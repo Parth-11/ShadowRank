f9ed3342aebdbc0fd4572b8e648d9cb56f0b9b1b
Parth-11 <parth12.mgg@gmail.com>
2024-07-22 23:42:53 +0530

    Added a quest builder bottom modal sheet

--- lib/screens/quest_page.dart
+++ lib/screens/quest_page.dart
@@ -1,13 +1,40 @@
 import "package:architect_system_app/components/QuestPage/task_tile.dart";
 import "package:flutter/material.dart";
 
-class QuestPage extends StatelessWidget {
+class QuestPage extends StatefulWidget {
   static const String id = 'Quest Page';
   const QuestPage({super.key});
 
   @override
+  State<QuestPage> createState() => _QuestPageState();
+}
+
+class _QuestPageState extends State<QuestPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: Column(
+                    children: [
+                      const Text('Quest builder'),
+                      const TextField(),
+                      const TextField(),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Add to Quest List'),
+                      )
+                    ],
+                  ),
+                );
+              });
+        },
+        child: const Icon(Icons.edit),
+      ),
       body: Stack(
         fit: StackFit.expand,
         children: [
@@ -29,7 +56,10 @@ class QuestPage extends StatelessWidget {
                   //Text and buttons
                   const Row(),
                   const Divider(),
-                  const TaskTile(task: 'Gym',taskQuantity: '60min',),
+                  const TaskTile(
+                    task: 'Gym',
+                    taskQuantity: '60min',
+                  ),
                 ],
               ),
             ),
